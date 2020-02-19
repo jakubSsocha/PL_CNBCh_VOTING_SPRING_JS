80bc1552ab6abdfe7286648bbedb30585651a6ec
jakubSsocha <jakub.socha@wp.pl>
2020-02-19 16:26:02 +0100

    Errors in log

--- src/main/java/pl/edu/uw/cnbch/voting/services/implementations/ErrorMessageServiceImpl.java
+++ src/main/java/pl/edu/uw/cnbch/voting/services/implementations/ErrorMessageServiceImpl.java
@@ -1,25 +1,45 @@
 package pl.edu.uw.cnbch.voting.services.implementations;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import pl.edu.uw.cnbch.voting.models.viewDTO.MessageDTO;
 import pl.edu.uw.cnbch.voting.services.ErrorMessageService;
+import pl.edu.uw.cnbch.voting.services.MainService;
 
 @Service
 public class ErrorMessageServiceImpl implements ErrorMessageService {
 
     private final String MODEL_ATTRIBUTE_NAME = "message";
     private final String MESSAGE_TYPE = "error";
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final MainService mainService;
+
+    @Autowired
+    public ErrorMessageServiceImpl(MainService mainService) {
+        this.mainService = mainService;
+    }
 
     @Override
-    public Model addMessageTo(Model model, String message) {
-
+    public Model addMessageTo(Model model, String message){
+        saveMessageIntoLog(message);
         model.addAttribute(MODEL_ATTRIBUTE_NAME,
                 new MessageDTO.Builder()
                         .text(message)
                         .type(MESSAGE_TYPE)
                         .build());
         return model;
+    }
+
+    private void saveMessageIntoLog(String message){
+        String errorMessage = new StringBuilder()
+                .append("Error message: ")
+                .append(message)
+                .toString();
+        logger.error(errorMessage);
     }
 
 
